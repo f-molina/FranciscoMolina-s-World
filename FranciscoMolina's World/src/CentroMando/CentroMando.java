3d4230a975ce50cc14d39334230370242cd06054
Francisco Molina <00300917@uca.edu.sv>
2018-06-06 02:31:33 -0600

    Creacion centro de mando

---
+++ FranciscoMolina's World/src/CentroMando/CentroMando.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CentroMando;
+
+/**
+ *
+ * @author fmolina
+ */
+public class CentroMando {
+    
+    int health = 1000;
+    int recurso1, recurso2, recurso3, lim1, lim2, lim3;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getRecurso1() {
+        return recurso1;
+    }
+
+    public void setRecurso1(int recurso1) {
+        this.recurso1 = recurso1;
+    }
+
+    public int getRecurso2() {
+        return recurso2;
+    }
+
+    public void setRecurso2(int recurso2) {
+        this.recurso2 = recurso2;
+    }
+
+    public int getRecurso3() {
+        return recurso3;
+    }
+
+    public void setRecurso3(int recurso3) {
+        this.recurso3 = recurso3;
+    }
+
+    public int getLim1() {
+        return lim1;
+    }
+
+    public void setLim1(int lim1) {
+        this.lim1 = lim1;
+    }
+
+    public int getLim2() {
+        return lim2;
+    }
+
+    public void setLim2(int lim2) {
+        this.lim2 = lim2;
+    }
+
+    public int getLim3() {
+        return lim3;
+    }
+
+    public void setLim3(int lim3) {
+        this.lim3 = lim3;
+    }
+    
+    
+    
+}
