bb64267facef8b48c45045b2f9400fabe4bccb9f
Francisco Molina <00300917@uca.edu.sv>
2018-06-09 19:02:41 -0600

    Se agregaron getter y setter

--- FranciscoMolina's World/src/Juego/Jugador.java
+++ FranciscoMolina's World/src/Juego/Jugador.java
@@ -5,6 +5,10 @@
  */
 package Juego;
 import CentroMando.CentroMando;
+import NintendoResources.Acero;
+import NintendoResources.Cristal;
+import NintendoResources.Mushrooms;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -15,12 +19,20 @@ public class Jugador {
     
     public String nombre;
     public CentroMando cm;
+    public  ArrayList<Acero> r1;
+    public  ArrayList<Cristal> r2;
+    public  ArrayList<Mushrooms> r3;
+    
 
     public Jugador() {
     }
-    public Jugador(String nombre, CentroMando cm) {
+
+    public Jugador(String nombre, CentroMando cm, ArrayList<Acero> r1, ArrayList<Cristal> r2, ArrayList<Mushrooms> r3) {
         this.nombre = nombre;
         this.cm = cm;
+        this.r1 = r1;
+        this.r2 = r2;
+        this.r3 = r3;
     }
 
     public String getNombre() {
@@ -38,24 +50,54 @@ public class Jugador {
     public void setCm(CentroMando cm) {
         this.cm = cm;
     }
+
+    public ArrayList<Acero> getR1() {
+        return r1;
+    }
+
+    public void setR1(ArrayList<Acero> r1) {
+        this.r1 = r1;
+    }
+
+    public ArrayList<Cristal> getR2() {
+        return r2;
+    }
+
+    public void setR2(ArrayList<Cristal> r2) {
+        this.r2 = r2;
+    }
+
+    public ArrayList<Mushrooms> getR3() {
+        return r3;
+    }
+
+    public void setR3(ArrayList<Mushrooms> r3) {
+        this.r3 = r3;
+    }   
     
     public void addJugador(){
         CentroMando m1 = new CentroMando();
+        ArrayList<Acero> r1 =new ArrayList<Acero>();
+        ArrayList<Cristal> r2 =new ArrayList<Cristal>();
+        ArrayList<Mushrooms> r3 =new ArrayList<Mushrooms>();
         Scanner leer = new Scanner(System.in);
         System.out.println("Jugador 1, digite su nombre: ");
         String player1 = leer.nextLine();
-        Jugador j1 = new Jugador(player1, m1);
-        System.out.println("Jugador 1: "+j1.getNombre()+" Vida centro mando: "+j1.cm.getHealth());
+        Jugador j1 = new Jugador(player1, m1, r1, r2, r3);
+        System.out.println("Jugador 1: "+j1.getNombre()+"\nVida centro mando: "+j1.cm.getHealth());
         Menu m = new Menu(j1);
         m.MenuRaza();
     }
     
     public void addJugador2(){
         CentroMando m2 = new CentroMando();
+        ArrayList<Acero> r1 =new ArrayList<Acero>();
+        ArrayList<Cristal> r2 =new ArrayList<Cristal>();
+        ArrayList<Mushrooms> r3 =new ArrayList<Mushrooms>();
         Scanner leer = new Scanner(System.in);
         System.out.println("Jugador 2, digite su nombre: ");
         String player2 = leer.nextLine();
-        Jugador j2 = new Jugador(player2, m2);
+        Jugador j2 = new Jugador(player2, m2, r1, r2, r3);
         System.out.println("Jugador 2: "+j2.getNombre()+" Vida centro mando: "+j2.cm.getHealth());
         Menu m = new Menu(j2);
         m.MenuRaza();
