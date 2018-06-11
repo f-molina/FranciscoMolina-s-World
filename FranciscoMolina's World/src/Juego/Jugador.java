df4bc28b550581f7b63f0a65cd5315007048e5ba
Francisco Molina <00300917@uca.edu.sv>
2018-06-10 22:36:48 -0600

    Se agregaron getter y setter de Resource y se continuo editando addJugador

--- FranciscoMolina's World/src/Juego/Jugador.java
+++ FranciscoMolina's World/src/Juego/Jugador.java
@@ -5,10 +5,8 @@
  */
 package Juego;
 import CentroMando.CentroMando;
-import NintendoResources.Acero;
-import NintendoResources.Cristal;
-import NintendoResources.Mushrooms;
-import java.util.ArrayList;
+/*import java.util.ArrayList;
+import java.util.Iterator;*/
 import java.util.Scanner;
 
 /**
@@ -19,13 +17,39 @@ public class Jugador {
     
     public String nombre;
     public CentroMando cm;
-    public  ArrayList<Acero> r1;
-    public  ArrayList<Cristal> r2;
-    public  ArrayList<Mushrooms> r3;
-    
-    public Acero a;
-    public Cristal c;
-    public Mushrooms m;
+    public Resource res1, res2, res3;
+
+    public Jugador(String nombre, CentroMando cm, Resource res1, Resource res2, Resource res3) {
+        this.nombre = nombre;
+        this.cm = cm;
+        this.res1 = res1;
+        this.res2 = res2;
+        this.res3 = res3;
+    }
+
+    public Resource getRes1() {
+        return res1;
+    }
+
+    public void setRes1(Resource res1) {
+        this.res1 = res1;
+    }
+
+    public Resource getRes2() {
+        return res2;
+    }
+
+    public void setRes2(Resource res2) {
+        this.res2 = res2;
+    }
+
+    public Resource getRes3() {
+        return res3;
+    }
+
+    public void setRes3(Resource res3) {
+        this.res3 = res3;
+    }
 
     public String getNombre() {
         return nombre;
@@ -42,123 +66,43 @@ public class Jugador {
     public void setCm(CentroMando cm) {
         this.cm = cm;
     }
-
-    public Acero getA() {
-        return a;
-    }
-
-    public void setA(Acero a) {
-        this.a = a;
-    }
-
-    public Cristal getC() {
-        return c;
-    }
-
-    public void setC(Cristal c) {
-        this.c = c;
-    }
-
-    public Mushrooms getM() {
-        return m;
-    }
-
-    public void setM(Mushrooms m) {
-        this.m = m;
-    }
-
-    public Jugador(String nombre, CentroMando cm, Acero a, Cristal c, Mushrooms m) {
-        this.nombre = nombre;
-        this.cm = cm;
-        this.a = a;
-        this.c = c;
-        this.m = m;
-    }
-
+    
     public Jugador() {
     }
-
-    /*public Jugador(String nombre, CentroMando cm, ArrayList<Acero> r1, ArrayList<Cristal> r2, ArrayList<Mushrooms> r3) {
-        this.nombre = nombre;
-        this.cm = cm;
-        this.r1 = r1;
-        this.r2 = r2;
-        this.r3 = r3;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public CentroMando getCm() {
-        return cm;
-    }
-
-    public void setCm(CentroMando cm) {
-        this.cm = cm;
-    }
-
-    public ArrayList<Acero> getR1() {
-        return r1;
-    }
-
-    public void setR1(ArrayList<Acero> r1) {
-        this.r1 = r1;
-    }
-
-    public ArrayList<Cristal> getR2() {
-        return r2;
-    }
-
-    public void setR2(ArrayList<Cristal> r2) {
-        this.r2 = r2;
-    }
-
-    public ArrayList<Mushrooms> getR3() {
-        return r3;
-    }
-
-    public void setR3(ArrayList<Mushrooms> r3) {
-        this.r3 = r3;
-    }   */
     
     public void addJugador(){
         CentroMando m1 = new CentroMando();
-        Acero a = new Acero();
-        Cristal c = new Cristal();
-        Mushrooms m = new Mushrooms();
-        /*ArrayList<Acero> r1 =new ArrayList<Acero>();
-        ArrayList<Cristal> r2 =new ArrayList<Cristal>();
-        ArrayList<Mushrooms> r3 =new ArrayList<Mushrooms>();*/
+        Resource r1 = new Resource();
+        Resource r2 = new Resource();
+        Resource r3 = new Resource();
         Scanner leer = new Scanner(System.in);
         System.out.println("Jugador 1, digite su nombre: ");
         String player1 = leer.nextLine();
-        Jugador j1 = new Jugador(player1, m1, a, c, m);
-        System.out.println("Jugador 1: "+j1.getNombre()+"\nVida centro mando: "+j1.cm.getHealth());
+        Jugador j1 = new Jugador(player1, m1, r1, r2, r3);
+        /*ArrayList<Jugador> jug1 = new ArrayList<Jugador>();
+        jug1.add(j1);
+        Iterator<Jugador> it = jug1.iterator();
+        while(it.hasNext()){
+            Jugador elemento = it.next();
+            System.out.println("hola:"+elemento);
+        }*/
+        System.out.println("Jugador 1: "+j1.getNombre()+"\nVida centro mando: "+j1.getCm().getHealth());
         Menu me = new Menu(j1);
-        me.MenuRaza();
+        me.MenuRaza(j1); 
     }
     
     public void addJugador2(){
         CentroMando m2 = new CentroMando();
-        int cantidad=0;
-        Acero a = new Acero();
-        Cristal c = new Cristal();
-        Mushrooms mu = new Mushrooms();
-        ArrayList<Acero> r1 =new ArrayList<Acero>();
-        ArrayList<Cristal> r2 =new ArrayList<Cristal>();
-        ArrayList<Mushrooms> r3 =new ArrayList<Mushrooms>();
+        Resource r1 = new Resource();
+        Resource r2 = new Resource();
+        Resource r3 = new Resource();
         Scanner leer = new Scanner(System.in);
         System.out.println("Jugador 2, digite su nombre: ");
         String player2 = leer.nextLine();
-        Jugador j2 = new Jugador(player2, m2, a, c, mu);
+        Jugador j2 = new Jugador(player2, m2, r1, r2, r3);
         System.out.println("Jugador 2: "+j2.getNombre()+" Vida centro mando: "+j2.cm.getHealth());
         Menu m = new Menu(j2);
-        m.MenuRaza();
+        m.MenuRaza(j2);
 
     }
     
