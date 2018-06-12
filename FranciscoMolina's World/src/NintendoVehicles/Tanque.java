aa28797e491b67006c9e87a4a87417fa4c107d65
Francisco Molina <00300917@uca.edu.sv>
2018-06-11 22:12:58 -0600

    Se agregaron getter y setter, y se termino metodo construir

--- FranciscoMolina's World/src/NintendoVehicles/Tanque.java
+++ FranciscoMolina's World/src/NintendoVehicles/Tanque.java
@@ -5,6 +5,8 @@
  */
 package NintendoVehicles;
 
+import AbstractFactory.AbstractFactory;
+import AbstractFactory.FactoryProducer;
 import Juego.Jugador;
 import Nintendo.Nintendo;
 
@@ -14,12 +16,76 @@ import Nintendo.Nintendo;
  */
 public class Tanque implements Nintendo{
     
-    int health;
-    int damage;
+    public int health = 250;
+    public int damage = 150;
+    public int costo = 125;
     int cantidad;
-    
+    public Jugador jugador;
+
+    public Tanque() {
+    }
+
+    public Tanque(Jugador jugador) {
+        this.jugador = jugador;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getCosto() {
+        return costo;
+    }
+
+    public void setCosto(int costo) {
+        this.costo = costo;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Jugador getJugador() {
+        return jugador;
+    }
+
+    public void setJugador(Jugador jugador) {
+        this.jugador = jugador;
+    }
+
     @Override
     public void construir(Jugador jugador){
+        int total, total1;
+        AbstractFactory nintendo = FactoryProducer.getFactory("Nintendo");
+        Nintendo tanque = nintendo.getNintendo("Tanque");
+        Tanque ta = new Tanque(jugador);
+        if(ta.getJugador().getCm().getRecurso1() >= costo 
+           && ta.getJugador().getCm().getRecurso2() >= costo){
+            total = ta.getJugador().getCm().getRecurso1()-costo;  
+            ta.getJugador().getCm().setRecurso1(total);
+            total1 = ta.getJugador().getCm().getRecurso2()-costo;
+            ta.getJugador().getCm().setRecurso2(total1);
+            System.out.println("Construccion realizada");
+            ta.getJugador().getV1().add(tanque);
+        }else{
+            System.out.println("No tiene recursos suficientes");
+        }
     }
     
     @Override
