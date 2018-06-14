d7eb4aa4fa65af0dbe1d09e696b3117cf4d340f5
Francisco Molina <00300917@uca.edu.sv>
2018-06-13 22:36:43 -0600

    se termino entrenar

--- FranciscoMolina's World/src/XboxArmy/Locust.java
+++ FranciscoMolina's World/src/XboxArmy/Locust.java
@@ -5,6 +5,7 @@
  */
 package XboxArmy;
 
+import Juego.Jugador;
 import Xbox.Xbox;
 
 /**
@@ -13,40 +14,109 @@ import Xbox.Xbox;
  */
 public class Locust implements Xbox{
     
-    int health;
-    int damage;
+    public int damage =75;
+    public int health = 75;
+    public int costo = 75;
     int cantidad;
-    
-    @Override
-    public int getCantidad(){
-        return cantidad;
+    public Jugador jugador;
+    boolean estado=false;
+
+    public Locust() {
     }
-    
-    @Override
-    public void setCantidad(int newCantidad){
-        cantidad = newCantidad;
+
+    public Locust(Jugador jugador) {
+        this.jugador = jugador;
     }
-    
-    @Override
-    public void construir(){
+
+    public int getDamage() {
+        return damage;
     }
-    
-    @Override
-    public void recoger(){
+
+    public void setDamage(int damage) {
+        this.damage = damage;
     }
-    
-    @Override
-    public int getLife(){
+
+    public int getHealth() {
         return health;
     }
-    
-    @Override
-    public void setLife(int newLife){
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getCosto() {
+        return costo;
+    }
+
+    public void setCosto(int costo) {
+        this.costo = costo;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Jugador getJugador() {
+        return jugador;
+    }
+
+    public void setJugador(Jugador jugador) {
+        this.jugador = jugador;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
     }
     
+    @Override
+    public void construir(Jugador jugador){
+    }
+    
+    @Override
+    public int recoger(){
+        return cantidad;
+    }
+
     @Override
     public int getAttack(){
         return damage;
+    }
+    
+    @Override
+    public boolean entrenar(Jugador jugador){
+        int total, total1;
+        Locust h = new Locust(jugador);
+        if(h.getJugador().getCm().getRecurso1() >= costo 
+           && h.getJugador().getCm().getRecurso3() >= costo && estado==false){
+            total = h.getJugador().getCm().getRecurso1()-costo;  
+            h.getJugador().getCm().setRecurso1(total);
+            total1 = h.getJugador().getCm().getRecurso3()-costo;
+            h.getJugador().getCm().setRecurso3(total1);
+            setEstado(true);
+            System.out.println("Se entrenaron Locust");
+        }else{
+            System.out.println("No tiene recursos suficientes");
+            setEstado(false);
+        }
+        return estado;
+    }
+    
+    @Override
+    public boolean estado(){
+        return estado;
+    }
+    
+    @Override
+    public void generar(Jugador jugador){
+        
     }
     
 }
--- FranciscoMolina's World/src/XboxArmy/MasterChief.java
+++ FranciscoMolina's World/src/XboxArmy/MasterChief.java
@@ -5,6 +5,7 @@
  */
 package XboxArmy;
 
+import Juego.Jugador;
 import Xbox.Xbox;
 
 /**
@@ -13,40 +14,108 @@ import Xbox.Xbox;
  */
 public class MasterChief implements Xbox{
     
-    int health;
-    int damage;
+    public int damage =75;
+    public int health = 75;
+    public int costo = 75;
     int cantidad;
-    
-    @Override
-    public int getCantidad(){
-        return cantidad;
+    public Jugador jugador;
+    boolean estado=false;
+
+    public MasterChief() {
     }
-    
-    @Override
-    public void setCantidad(int newCantidad){
-        cantidad = newCantidad;
+
+    public MasterChief(Jugador jugador) {
+        this.jugador = jugador;
     }
-    
-    @Override
-    public void construir(){
+
+    public int getDamage() {
+        return damage;
     }
-    
-    @Override
-    public void recoger(){
+
+    public void setDamage(int damage) {
+        this.damage = damage;
     }
-    
-    @Override
-    public int getLife(){
+
+    public int getHealth() {
         return health;
     }
-    
-    @Override
-    public void setLife(int newLife){
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getCosto() {
+        return costo;
+    }
+
+    public void setCosto(int costo) {
+        this.costo = costo;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Jugador getJugador() {
+        return jugador;
+    }
+
+    public void setJugador(Jugador jugador) {
+        this.jugador = jugador;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
     }
     
+    @Override
+    public void construir(Jugador jugador){
+    }
+    
+    @Override
+    public int recoger(){
+        return cantidad;
+    }
+
     @Override
     public int getAttack(){
         return damage;
     }
     
+    @Override
+    public boolean entrenar(Jugador jugador){
+        int total, total1;
+        MasterChief h = new MasterChief(jugador);
+        if(h.getJugador().getCm().getRecurso1() >= costo 
+           && h.getJugador().getCm().getRecurso3() >= costo && estado==false){
+            total = h.getJugador().getCm().getRecurso1()-costo;  
+            h.getJugador().getCm().setRecurso1(total);
+            total1 = h.getJugador().getCm().getRecurso3()-costo;
+            h.getJugador().getCm().setRecurso3(total1);
+            setEstado(true);
+            System.out.println("Se entreno a Master Chief");
+        }else{
+            System.out.println("No tiene recursos suficientes");
+            setEstado(false);
+        }
+        return estado;
+    }
+    
+    @Override
+    public boolean estado(){
+        return estado;
+    }
+    
+    @Override
+    public void generar(Jugador jugador){
+        
+    }
 }
